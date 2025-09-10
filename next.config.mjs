701435a5e7d98a2589f08a073d7e096d77a35bb5
Zeyad Nafea <s-zeyad.nafea@zewailcity.edu.eg>
2025-09-10 18:51:47 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  basePath: isProd ? "/portfolio-website" : "",
+  assetPrefix: isProd ? "/portfolio-website/" : "",
+  images: {
+    unoptimized: true, // required for GitHub Pages
+  },
+};
+
+module.exports = nextConfig;
